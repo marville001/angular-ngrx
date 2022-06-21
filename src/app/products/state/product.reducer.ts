c35c6e51013b7f8817219e9b9eb6ce9a27e049d2
Martin Mwangi <mwangimartin1904@gmail.com>
2022-06-21 23:25:14 +0300

    Strongly typing state

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,9 +1,21 @@
 import { createAction, createReducer, on } from "@ngrx/store";
+import * as AppState from "../../state/app.state"
 
+import { Product } from "../product"
+
+export interface State extends AppState.State {
+	products: ProductState;
+}
+
+export interface ProductState {
+	showProductCode: boolean;
+	currentProduct: Product;
+	products: Product[]
+}
 
 export const productReducer = createReducer(
-	{ showProductCode: true },
-	on(createAction('[Product] Toggle Product Code'), state => {	
+	{ showProductCode: true } as ProductState,
+	on(createAction('[Product] Toggle Product Code'), state => {
 		return {
 			...state,
 			showProductCode: !state.showProductCode
